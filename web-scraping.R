65bc22c2ccf3453a66e3e41f566c299de112f8d1
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-17 22:28:58 +0200

    Update web-scraping.R

--- web-scraping.R
+++ web-scraping.R
@@ -7,6 +7,9 @@ library(stringr)  # string manipulation
 library(rvest)
 library(magrittr)
 library(rjson)
+library(rtimes) # NY times api
+library(httr)  ## working with urls and api
+library(jsonlite)
 # set url for online csv file
 url <- "https://s3.amazonaws.com/bsp-ocsit-prod-east-appdata/datagov/wordpress/agency-participation.csv"
 
@@ -15,8 +18,6 @@ data_gov <- read.csv(url, stringsAsFactors = FALSE)
 head(data_gov)
 
 ##### reading excel from online
-
-
 # the url for the online Excel file
 url <- "http://www.huduser.org/portal/datasets/fmr/fmr2015f/FY2015F_4050_Final.xls"
 
@@ -200,8 +201,58 @@ bls_table9 <- readHTMLTable(tablenodes[[3]], colClasses = c("character", "charac
 
 
 ######## Scraping data using API
-# rnoaa API
+# rtimes API
+api_key <- "tUIXCud365uQWHHqkNBYy4cwWhWLWMVM"
+articles <- as_search(q = "elections", key = api_key)
+articles$meta
+articles$data$web_url
+articles$data$keywords
+articles$data[[3]]
 
+
+####### httr for api
+####### climate change data
+#### Global Warming live graphs and API
+### Retrive methane data
+## define endpoint url
+endpoint_url <- "https://global-warming.org/api/methane-api"
+# request the data
+methane_data <- GET(endpoint_url)
+
+# retrive content of data
+content(methane_data)
+methane_data_contx <- httr::text_content(methane_data)
+methane_text <- content(methane_data, as = 'text') # retrieve content as text
+methane_raw <- content(methane_data, as = 'raw') #retrieve data as ras
+methane_parsed <- content(methane_data, as = 'parsed')
+
+## convert from json to r object
+methane_fromJson <- fromJSON(methane_text) 
+
+# convert to  dataframe
+(methane_dataframe <- as.data.frame(methane_fromJson))
+
+### retrive temperature anomaly data
+temp_url <- "https://global-warming.org/api/temperature-api"
+
+# retrives data from API and convert from JSON to R object
+temp_anomaly_data <- fromJSON(temp_url)
+temp_anomaly_dataframe <- as.data.frame(temp_anomaly_data$result)
+
+# retrieve CO2 data from API
+co2_url <- "https://global-warming.org/api/co2-api"
+co2_from_json <- fromJSON(co2_url)
+co2_dataframe <- as.data.frame(co2_from_json)
+
+## retrieve NO2 data from API
+no2_url <- "https://global-warming.org/api/nitrous-oxide-api"
+no2_json <- fromJSON(no2_url)
+no2_dataframe <- as.data.frame(no2_json)
+
+## retrive average monthly arctic sea ice extent
+arctic_sea_extent <- "https://global-warming.org/api/arctic-api"
+arctic_sea_extent_json <- fromJSON(arctic_sea_extent)
+arctic_sea_dataframe <- as.data.frame(arctic_sea_extent_json$result)
 
 ################################
 terra_viva <- read_html("https://terravivagrants.org/funding-news/")
